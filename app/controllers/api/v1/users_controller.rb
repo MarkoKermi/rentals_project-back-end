fc50dd255bec812a25ee4531ec33f1d373f5820f
Marko Kermiciev <kermicievmarko@gmail.com>
2023-06-19 08:57:13 +0200

    setup user controler and model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,37 @@
-module API
-  class UsersController < ApplicationController
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def create
+        user = User.new(user_params)
+
+        if user.save
+          render_created_response(user)
+        else
+          render_error_response(user.errors)
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:username, :password)
+      end
+
+      def render_created_response(user)
+        render json: {
+          user: {
+            id: user.id,
+            username: user.username
+          },
+          message: 'User created successfully.'
+        }, status: :created
+      end
+
+      def render_error_response(errors)
+        render json: {
+          error: errors.full_messages.join(' ')
+        }, status: :unprocessable_entity
+      end
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 # Creates the user model
 class User < ApplicationRecord
+  has_many :vespas, dependent :destroy
+  has_many :reservations, dependent :destroy
+
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, lenght: { minimum: 6 }  
 end

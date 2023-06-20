c58f7b12dedd4552c2bb1898d2f3777d7e1280e1
Marko Kermiciev <kermicievmarko@gmail.com>
2023-06-20 08:30:17 +0200

    setup the routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # Creates the user model
 class User < ApplicationRecord
-  has_many :vespas
-  has_many :reservations
+  has_many :vespas, dependent: :destroy
+  has_many :reservations, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }

656c18901dc51a4f5f7bff7a0895f095b5a9e2bc
Yash <yash.solo.000@gmail.com>
2023-06-21 14:58:58 +0600

    fix linter

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,8 +1,7 @@
 module Api
-    module V1
-      class BaseController < ApplicationController
-        # Add any shared code or authentication logic here
-      end
+  module V1
+    class BaseController < ApplicationController
+      # Add any shared code or authentication logic here
     end
   end
-  
+end
